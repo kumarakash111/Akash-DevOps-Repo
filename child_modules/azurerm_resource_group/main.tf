9949ce20f1dc83701df77fda35f41ce099d0914f
kumarakash111 <akgharlach@gmail.com>
2025-06-30 13:17:50 +0530

    adding resource group and notes

--- child_modules/azurerm_resource_group/main.tf
+++ child_modules/azurerm_resource_group/main.tf
@@ -1,3 +1,13 @@
+# adding one more resoiurce group to the module
+# This module creates an Azure Resource Group
+
+
+resource "azurerm_resource_group" "akkirg33" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+
 resource "azurerm_resource_group" "akkirg" {
   name     = var.resource_group_name
   location = var.location
