ff4ee0ed08ace60519efc4b9e8897ef7efd1a6c1
kumarakash111 <akgharlach@gmail.com>
2025-07-28 17:04:24 +0530

    adding subnet module

--- Parent_modules/main.tf
+++ Parent_modules/main.tf
@@ -23,6 +23,14 @@ module "subnet" {
   address_prefixes     = ["10.0.0.0/24"]
 }
 
+module "subnet1" {
+  depends_on           = [module.virtual_network]
+  source               = "../child_modules/azurerm_subnet"
+  subnet_name          = "aktestsubnet1"
+  resource_group_name  = "aktestrg"
+  virtual_network_name = "aktestvnet"
+  address_prefixes     = ["10.0.1.0/24"]
+}
 module "public_ip" {
   depends_on              = [module.resource_group]
   source                  = "../Child_modules/azurerm_public_ip"
